863714d26e7956b462a2b735cbe312efe3b4cc8c
Little-monster-HU <826398415@qq.com>
2021-03-08 17:26:34 +0800

    Create Program.cs

---
+++ 3.5test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._5test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
